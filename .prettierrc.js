e416d200d8ed4b9fd0f26489a94c825e719d87dc
ngoctienle <ngoctienle.2399@gmail.com>
2023-06-15 19:03:48 +0700

    feat(*): initial source setup

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+const config = {
+  semi: false,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  endOfLine: 'auto'
+}
+
+module.exports = config
